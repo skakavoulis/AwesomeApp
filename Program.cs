2c1ecc27d6db3bde022a2d6b37d3708292320c6e
Sotiris Kakavoulis <sotiriskakavoulis@gmail.com>
2023-01-26 20:17:33 +0200

    Add error handling & rate limiting

--- Program.cs
+++ Program.cs
@@ -1,16 +1,78 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddPolicy("api", httpContext =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: httpContext.User.Identity?.Name
+                ?? httpContext.Connection.RemoteIpAddress?.ToString()
+                ?? Guid.NewGuid().ToString()
+                ?? "fixed_key",
+            factory: partition => new FixedWindowRateLimiterOptions
+            {
+                AutoReplenishment = true,
+                PermitLimit = 10,
+                QueueLimit = 0,
+                Window = TimeSpan.FromMinutes(1),
+            }));
+    options.OnRejected = (context, cancellationToken) =>
+    {
+        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+        context.HttpContext.Response.ContentType = Application.Json;
+        context.HttpContext.Response.WriteAsJsonAsync(new
+        {
+            Error = "Too many requests"
+        });
+
+        return ValueTask.CompletedTask;
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+app.UseExceptionHandler(errorApp =>
 {
-    app.UseExceptionHandler("/Home/Error");
-}
+    errorApp.Run(async context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = Application.Json;
+
+        var feature = context.Features.Get<IExceptionHandlerPathFeature>();
+        var error = feature?.Error;
+
+        if (error is ApplicationException)
+        {
+            await context.Response.WriteAsJsonAsync(new
+            {
+                Error = error.Message
+            });
+        }
+        else
+        {
+            await context.Response.WriteAsJsonAsync(new
+            {
+                Error = "Something went wrong. Please try again later."
+            });
+        }
+    });
+});
 app.UseStaticFiles();
+
+app.UseRateLimiter();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllerRoute(
     name: "default",
